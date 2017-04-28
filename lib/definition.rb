cc0050057e57839df72c38516839f5f338f3ace6
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-04-28 11:28:34 -0700

    all rspecs for definition and word passing

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,3 +1,30 @@
 class Definition
 
+attr_accessor(:explanation)
+@@definitions = []
+
+  def initialize(attributes)
+    @explanation = attributes.fetch(:explanation)
+    @id = @@definitions.length + 1
+  end
+
+  define_method (:explanation) do
+    @explanation
+  end
+
+  def self.all
+    @@definitions
+  end
+
+  def save
+    @@definitions.push(self)
+  end
+
+  def id
+    @id
+  end
+
+  def self.clear
+    @@definitions = []
+  end
 end
---
+++ spec/definition_spec.rb
@@ -0,0 +1,49 @@
+require('rspec')
+require('definition')
+
+describe(Definition) do
+
+  before do
+    Definition.clear()
+  end
+
+  describe("#explanation") do
+    it("returns the definition back that was inputted") do
+      test_word = Definition.new({:explanation=> "a place for a fire with a chimney"})
+      expect(test_word.explanation()).to(eq("a place for a fire with a chimney"))
+      end
+  end
+
+describe(".all") do
+   it("shows what is inside the array") do
+     expect(Definition.all()).to(eq([]))
+   end
+ end
+
+ describe(".clear") do
+   it("clears the array") do
+     expect(Definition.all()).to(eq([]))
+   end
+ end
+
+ describe("#save") do
+   it("saves the definition to the array") do
+     definition_test = Definition.new({:explanation=> "object used to support someone's head"})
+     definition_test.save()
+     definition2_test = Definition.new({:explanation=> "colour between blue and yellow"})
+     definition2_test.save()
+     expect(Definition.all()).to(eq([definition_test, definition2_test]))
+   end
+ end
+
+ describe("#id") do
+    it("returns an id number of the word") do
+      definition_test = Definition.new({:explanation=> "object used to support someone's head"})
+      definition_test.save()
+      definition2_test = Definition.new({:explanation=> "colour between blue and yellow"})
+      definition2_test.save()
+      expect(definition_test.id()).to(eq(1))
+      expect(definition2_test.id()).to(eq(2))
+    end
+  end
+end
